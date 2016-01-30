85d28a6de225461fcecb8a0fb48b8f209432a0e0
Josh <bluemazaro@yahoo.com>
2016-01-30 00:55:24 -0800

    moved forms to main init file and fixed username & email form validation

--- Flask_Blog/__init__.py
+++ Flask_Blog/__init__.py
@@ -67,12 +67,61 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
+#
+
+# from forms import PostForm, LoginForm, SignupForm
+
+#
+
+from flask_wtf import Form
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from flask.ext.wtf.html5 import URLField
+from wtforms.validators import DataRequired, url, Length, Email, Regexp, EqualTo,\
+    url, ValidationError
+
+class PostForm(Form):
+    entry = StringField('Enter your review of the album:')
+    artist = StringField('Enter the artist that created this album:')
+    title = StringField('Enter the album title:')
+    rating = IntegerField('Rate the album out of 5:')
+
+class LoginForm(Form):
+    username = StringField('Your Username:', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+class SignupForm(Form):
+    username = StringField('Username',
+                    validators=[
+                        DataRequired(), Length(3, 80),
+                        Regexp('^[A-Za-z0-9_]{3,}$',
+                            message='Usernames consist of numbers, letters,'
+                                    'and underscores.')])
+    password = PasswordField('Password',
+                    validators=[
+                        DataRequired(),
+                        EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    email = StringField('Email',
+                    validators=[DataRequired(), Length(1, 120), Email()])
+
+    def validate_email(self, email_field):
+        if User.query.filter_by(email=email_field.data).first():
+            raise ValidationError('There is already a user with this email address.')
+
+    def validate_username(self, username_field):
+        if User.query.filter_by(username=username_field.data).first():
+          raise ValidationError('This username is already taken.')
+
+#
+
 # import views
+
+#
 
 from flask import render_template, url_for, redirect, flash
 from flask_login import login_required, login_user, logout_user, current_user
-
-from forms import PostForm, LoginForm, SignupForm
 
 @login_manager.user_loader
 def load_user(userid):
