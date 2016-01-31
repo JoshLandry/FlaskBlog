4eea8ee348fd8a356784d4b92332e792d4c90812
Josh <bluemazaro@yahoo.com>
2016-01-30 19:26:27 -0800

    fixed style problems with window resizing, began implementing Tag feature

--- Flask_Blog/__init__.py
+++ Flask_Blog/__init__.py
@@ -27,6 +27,11 @@ from sqlalchemy import desc
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
+tags = db.Table('entry_tag',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('entry_id', db.Integer, db.ForeignKey('blog_entry.id'))
+)
+
 class BlogEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.String(300), nullable=False)
@@ -35,6 +40,8 @@ class BlogEntry(db.Model):
     entry = db.Column(db.String(300))
     rating = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    tags = db.relationship('Tag', secondary=tags,
+                            backref=db.backref('entries', lazy='dynamic'))
 
     @staticmethod
     def newest(num):
@@ -67,6 +74,13 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False, unique=True, index=True)
+
+    def __repr__(self):
+        return self.name
 
 #
 
